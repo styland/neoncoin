d108ee2e1e13ac6bbce3245934cf65716f5fb66b
styland <rotimidamilola2011@gmail.com>
2025-07-18 15:05:37 -0700

    Fix: update after adding db or adjusting app.py

--- app.py
+++ app.py
@@ -1099,5 +1099,8 @@ def update_spin_availability(user_id):
     return spins, next_spin_time
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
